b5f92f3cb31d485eb4a101007950fe41beeb6760
diego-dsr <diego.saldanha@ymail.com>
2026-05-26 16:17:56 -0300

    Graficos prontos

--- MxH.py
+++ MxH.py
@@ -1,22 +1,24 @@
 import sys
+
+print(sys.executable)
+print(sys.version)
+
+import numpy
+print(numpy.__version__)
 sys.path.append(r'C:\Users\diego\OneDrive\Documentos\GitHub\AULA5')
-import magdynlab.utils.MxH_A
-medida_MxH = magdynlab.utils.MxH_A.MxH
+import MxH_A
+medida_MxH = MxH_A.MxH
 import sys
-#import magdynlab.utils.MxH_A
-#medida_MxH = magdynlab.utils.MxH_A.MxH
-#import magdynlab.utils.nVxH
-#ISHE = magdynlab.utils.nVxH.nVxH
 import matplotlib.pyplot as plt
 import numpy as np
 import glob
 import os
 
 # Leitura das medidas
-R23  = MxH(r'C:\Users\diego\OneDrive\Documentos\GitHub\AULA5\R23_002')
-R23A = MxH(r'C:\Users\diego\OneDrive\Documentos\GitHub\AULA5\R23A_001')
-R23B = MxH(r'C:\Users\diego\OneDrive\Documentos\GitHub\AULA5\R23B_002')
-R23C = MxH(r'C:\Users\diego\OneDrive\Documentos\GitHub\AULA5\R23C_002')
+R23  = medida_MxH(r'C:\Users\diego\OneDrive\Documentos\GitHub\AULA5\R23_002')
+R23A = medida_MxH(r'C:\Users\diego\OneDrive\Documentos\GitHub\AULA5\R23A_001')
+R23B = medida_MxH(r'C:\Users\diego\OneDrive\Documentos\GitHub\AULA5\R23B_002')
+R23C = medida_MxH(r'C:\Users\diego\OneDrive\Documentos\GitHub\AULA5\R23C_002')
 
 # Cria a figura
 plt.figure('MxH', figsize=(5, 4))
